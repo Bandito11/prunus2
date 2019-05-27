f135d6844cfb0d688c9c6335c31ed7046fc35f9b
Esteban Morales <red_bandit_11@hotmail.com>
2019-05-27 16:17:52 -0400

    .

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,6 +1,76 @@
-// import { Storage } from '@ionic/storage';
+import { Plugins, FilesystemDirectory, FilesystemEncoding } from '@capacitor/core';
+
+const { Filesystem } = Plugins;
 
 declare const localforage;
+
+// Implement the driver here.
+const fileDriver = {
+    _driver: 'capacitorFleDriver',
+    _initStorage: function (options) {
+        options;
+    },
+    clear: async function (callback) {
+        await Filesystem.deleteFile({
+            path: 'secrets/text.txt',
+            directory: FilesystemDirectory.Documents
+          });
+          callback(null);
+    },
+    getItem: async function (key, callback) {
+        const contents = await Filesystem.readFile({
+            path: `db/${key}.txt`,
+            directory: FilesystemDirectory.Documents,
+            encoding: FilesystemEncoding.UTF8
+        });
+        callback(contents);
+
+    },
+    iterate: function (iteratorCallback, successCallback) {
+        iteratorCallback();
+        successCallback();
+    },
+    key: function (n, callback) {
+        callback(n);
+    },
+    keys: function (callback) {
+        callback(null);
+    },
+    length: function (callback) {
+        callback(null);
+    },
+    removeItem: async function (key, callback) {
+        await Filesystem.deleteFile({
+            path: `.db/${key}.txt`,
+            directory: FilesystemDirectory.Documents
+        });
+        callback(null);
+    },
+    setItem: function (key, value, callback) {
+        try {
+            Filesystem.writeFile({
+                path: `db/${key}.txt`,
+                data: value,
+                directory: FilesystemDirectory.Documents,
+                encoding: FilesystemEncoding.UTF8
+            })
+        } catch (e) {
+            console.error('Unable to write file', e);
+        }
+        callback(null);
+    }
+}
+
+// Add the driver to localForage.
+localforage.defineDriver(fileDriver);
+
+localforage.setDriver([
+    localforage.fileDriver,
+    localforage.INDEXEDDB, 
+    localforage.WEBSQL, 
+    localforage.LOCALSTORAGE
+]);
+
 
 class Storage {
 
